b5dc32e7b52bef6103b635fe559d8954a0c9841f
pqtv <pqtv5698@gmail.com>
2024-03-12 00:23:08 +0700

    first commit

---
+++ ec2-module.tf
@@ -0,0 +1,5 @@
+# module "ec2-module" {
+#   source = "../../modules/ec2"
+#   #Can't change type or ami if use hardcoded code
+#   instancetype1 = "t2.large"
+# }
---
+++ sg-module.tf
@@ -0,0 +1,3 @@
+# module "ec2-sg" {
+#   source = "../../modules/sg"
+# }
